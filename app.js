66538f85ded7804024652667ad697ae948f57b85
seungguini <sl6369@nyu.edu>
2020-10-09 18:23:47 +0900

    incorporating gsap

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
+    // fires when user leaves 'origin' section to a 'direction'
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector('h1')
+        const t1 = new TimelineMax();
+        t1.fromTo(title, 0.5, {y: '50', opacity: 0}, {y:.0, opacity: 1});
+    }
 })
