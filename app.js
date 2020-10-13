24af3d9ba5ba977386aacdb869916f80e7ba58a2
seungguini <sl6369@nyu.edu>
2020-10-13 16:37:45 +0900

    working on plane animation

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var loadTimeline = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
+
+const tlPlane = gsap.timeline
+tlPlane.to(".plane", 3,
+    {y: '+=30', rotation: '-=3', ease:Power1.easeInOut})
+
+
+floatPlane();
