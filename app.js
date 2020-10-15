b1fce567c77edcf3a9f5d3c1160d4025ae01e576
seungguini <sl6369@nyu.edu>
2020-10-16 00:24:57 +0900

    plane and cloud animation on, need to work on smoothness and background color

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tlPlane = gsap.timeline
-tlPlane.to(".plane", 3,
-    {y: '+=30', rotation: '-=3', ease:Power1.easeInOut})
+/* timeline for bobbing airplane */
+var tlPlane = gsap.timeline({
+    defaults: {duration: 3, ease: "sine.inOut"},
+    repeat: -1
+});
+/* airplane bobbing animation */
+tlPlane.fromTo(".plane",{
+    y: '+=30',
+    rotation: '-=1'
+}, {
 
+    y: '-=30',
+    rotation: '+=1',
+})
+.to(".plane", {
+    y: '+=30',
+    rotation: '-=1',
+});
 
-floatPlane();
+gsap.to(".cloudf", 5 ,{
+    right: '125%',
+    stagger: {
+        each: 2,
+        repeat: -1
+    }
+});
+
+gsap.to(".cloudb", 10 ,{
+    right: '125%',
+    stagger: {
+        each: 2,
+        repeat: -1
+    }
+});
+
+/*
+var tlCloud = gsap.timeline({
+    repeat: -1,
+    defaults: {duration: 3}
+});
+
+tlCloud.to("#cloud1", {right: '125%'}, "<1")
+.to("#cloud2", {right: '125%'}, "<2")
+.to("#cloud3", {right: '125%'}, "<1.5"); */
