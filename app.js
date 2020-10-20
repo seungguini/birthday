48b14b41ab9c1927fd239cd540c02767e9c19975
seungguini <sl6369@nyu.edu>
2020-10-21 00:06:27 +0900

    hello messages work!

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
-let tlIntro = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".intro",
-        start: "center center",
-        end: "5000 center",
-        markers: true,
-        pin: true,
-        scrub: 3
-    }
+const hellos = ["hello there!", "안녕하세요!", "selamat pagi!", "今日は!", "bonjour!", "你好!"]
+
+let cursor = gsap.to('.cursor1', {duration: 1, opacity: 0, ease: "power2.inOut", repeat: -1});
+
+let masterTl = gsap.timeline();
+
+let tlIntroAnimate = gsap.timeline({
+    repeat: -1
+}).delay(3);
+
+hellos.forEach(word => {
+    let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 0});
+    tl.to('.hello', {
+        duration: 3, text: word, ease: "power4.out"
+    });
+    tlIntroAnimate.add(tl);
 });
+
+let tlPlease = gsap.timeline();
+tlPlease.from(".text2", {
+    opacity: '0%',
+    ease: 'power2.in',
+    duration: 3
+}).delay(30);
+
+
+
+
+
+
+
 
 /* timeline for bobbing airplane, with lagged scrubbing control
 let tlPlaneControl = gsap.timeline({
