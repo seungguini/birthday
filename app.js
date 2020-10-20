ef7b1edbdd4a203bede136e68d3d7160675f3460
seungguini <sl6369@nyu.edu>
2020-10-20 22:45:45 +0900

    ditched plane animation

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
-const words = ["Welcome Aboard", "to a new", "Adventure"];
-const ids= ["cursor1", "cursor2", "cursor3"]
-
-let cursor = gsap.to(".cursor", {
-    opacity: 0, ease: "power2.inOut", repeat:-1
+let tlIntro = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".intro",
+        start: "center center",
+        end: "5000 center",
+        markers: true,
+        pin: true,
+        scrub: 3
+    }
 });
 
-let masterTextTl = gsap.timeline()
-
-words.forEach(word => {
-    // 1. typewrite line of text 2. pause for a second before 'entering' (cursor animation)
-    let tl = gsap.timeline()
-    tl.to('.text', {
-        duration: 1,
-        value: word
-    })
-
-    masterTextTl.add(tl);
-});
-    
-
-/* IMMA NEST THIS SHIT */
-
-/* airplane bobbing animation (no control)*/
-
-
-/* timeline for bobbing airplane, with lagged scrubbing control*/
+/* timeline for bobbing airplane, with lagged scrubbing control
 let tlPlaneControl = gsap.timeline({
     scrollTrigger: {
         trigger: ".preload",
@@ -40,7 +25,7 @@ let tlPlaneControl = gsap.timeline({
     repeat: 1
 });
 
-/* airplane bobbing animation */
+/* airplane bobbing animation
 tlPlaneControl.fromTo(".plane",{
     y: '+=30',
     rotation: '-=1'
@@ -54,12 +39,4 @@ tlPlaneControl.fromTo(".plane",{
     rotation: '-=1',
 });
 
-/*
-var tlCloud = gsap.timeline({
-    repeat: -1,
-    defaults: {duration: 3}
-});
-
-tlCloud.to("#cloud1", {right: '125%'}, "<1")
-.to("#cloud2", {right: '125%'}, "<2")
-.to("#cloud3", {right: '125%'}, "<1.5"); */
+*/
