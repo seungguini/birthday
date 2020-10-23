98d59610f9c4ac5b011997a5e55ef84ccd948cee
seungguini <sl6369@nyu.edu>
2020-10-23 15:51:21 +0900

    round corners animation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
-const hellos = ["i love you!", "안녕하세요!", "selamat pagi!", "今日は!", "bonjour!", "你好!"]
+const hellos = ["i love you!", "사랑해!", "saya sayang kamu!", "今日は!", "Je t'aime!", "你好!"]
 
 let tlMaster = gsap.timeline();
 
@@ -12,15 +12,21 @@ tlMaster.to('.typing',{
     delay: 1.5
 })
 
+.to('.typing',{
+    borderRadius: "20",
+    ease: 'elastic',
+    duration: 3
+})
+
 // ENTER > AND _
 .to('.arrow', {
     opacity: 100,
     duration: 5,
     ease: 'power1.out',
-},">3")
+},"<2")
 
 // CURSOR
-.to('.cursor1', {duration: 1, opacity: 100, ease: "power2.inOut", repeat: -1, yoyo:true}, "<3");
+.to('.cursor1', {opacity: 100, ease: "power4.inOut", repeat: -1, yoyo:true, repeatDelay: 0.5}, "<2");
 
 let tlIntroAnimate = gsap.timeline({
     repeat: -1,
@@ -29,23 +35,23 @@ let tlIntroAnimate = gsap.timeline({
 hellos.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1});
     tl.to('.hello', {
-        duration: 2, text: word, ease: "power2.out"
-    });
+        duration: 2, text: word, ease: "power4.inOut"
+    }, ">0.5");
     tlIntroAnimate.add(tl);
 });
 
-tlMaster.add(tlIntroAnimate, "+=7");
+tlMaster.add(tlIntroAnimate, ">2")
 
-tlMaster
+// PLS SCROLL FLASH
 .to(".plsScroll", {
     opacity: 100,
     ease: 'power1.inOut',
     duration: 2,
     repeat: -1,
     yoyo: true
-});
+}, ">10");
 
-
+// pause animation upon scroll
 
 
 
