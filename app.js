173d3f74b1e933ffa2c2675dca73fe493e84e127
seungguini <sl6369@nyu.edu>
2020-10-24 23:56:54 +0900

    inserted user scroll block

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
-const hellos = ["i love you!", "사랑해!", "saya sayang kamu!", "今日は!", "Je t'aime!", "你好!"]
+// add this as something that plays if you don't type anything for a while..
+const hellos = ["i love you!", "사랑해!", "saya sayang kamu!", , "我爱你!", "Je t'aime!", "愛してる!"]
 
 let tlMaster = gsap.timeline();
 
@@ -16,7 +17,7 @@ tlMaster.to('.typing',{
     borderRadius: "20",
     ease: 'elastic',
     duration: 3
-})
+}, "<0.2")
 
 // ENTER > AND _
 .to('.arrow', {
@@ -26,36 +27,65 @@ tlMaster.to('.typing',{
 },"<2")
 
 // CURSOR
-.to('.cursor1', {opacity: 100, ease: "power4.inOut", repeat: -1, yoyo:true, repeatDelay: 0.5}, "<2");
+.to('.cursor1', 0.5, {
+    opacity: 100, 
+    ease: "power4.in", 
+    repeat: -1, 
+    yoyo:true, 
+    repeatDelay: 0.5}, "<1")
+
+// hello rika!
+let tl = gsap.timeline({repeat: 1, yoyo:true, repeatDelay: 1});
+tl.to('.hello', {
+    duration: 2, 
+    text: "hello rika <3", 
+    ease: "power4.inOut"
+},"<1");
+
+tlMaster.add(tl)
+
+.to("html, body", {
+    overflow: "visible"
+})
+
+// Add scroll trigger after "i love you" - fade screen and show please scroll
+
+// PLS SCROLL FLASH
+.to(".plsScroll", {
+    opacity: 100,
+    ease: 'power4.inOut',
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 0.7
+}, ">");
 
 let tlIntroAnimate = gsap.timeline({
-    repeat: -1,
-})
+    scrollTrigger: {
+        trigger: ".intro",
+        start: "center center",
+        end: "5000 top",
+        pin: ".intro",
+        scrub: 1,
+        markers: true
+    }
+});
 
 hellos.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1});
     tl.to('.hello', {
-        duration: 2, text: word, ease: "power4.inOut"
-    }, ">0.5");
+        duration: 2, 
+        text: word, 
+        ease: "power4.inOut"
+    });
     tlIntroAnimate.add(tl);
 });
 
 tlMaster.add(tlIntroAnimate, ">2")
 
-// PLS SCROLL FLASH
-.to(".plsScroll", {
-    opacity: 100,
-    ease: 'power1.inOut',
-    duration: 2,
-    repeat: -1,
-    yoyo: true
-}, ">10");
+
 
 // pause animation upon scroll
-
-
-
-
 
 /* timeline for bobbing airplane, with lagged scrubbing control
 let tlPlaneControl = gsap.timeline({
